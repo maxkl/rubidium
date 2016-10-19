bbd07814e96d6a89bc5bbdace202dd6ddba4be24
maxkl <max@maxkl.de>
2016-10-19 21:27:01 +0200

    string.h: strlen and strcpy

--- src/include/string.h
+++ src/include/string.h
@@ -7,3 +7,6 @@ int memcmp(const void *, const void *, size_t);
 void *memset(void *, int, size_t);
 void *memcpy(void * restrict, const void * restrict, size_t);
 void *memmove(void *, const void *, size_t);
+
+size_t strlen(const char *);
+char *strcpy(char *, const char *);
